957fc67ac12225d48d1abc51eab498290efe6e29
William J. Bowman <wjb@williamjbowman.com>
2020-12-12 22:22:54 -0800

    Add missing init file

--- init-spamassassin.sh
+++ init-spamassassin.sh
@@ -4,3 +4,4 @@ mkdir -p ~/.spamassassin/{lib,gpg}
 cp -r /etc/mail/spamassassin ~/.spamassassin/lib/
 sa-update --gpghomedir ~/.spamassassin/gpg/ --updatedir ~/.spamassassin/lib/
 cp user_prefs ~/.spamassassin/
+cp -f init.pre ~/.spamassassin/lib/
