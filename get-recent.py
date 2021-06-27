847d28be508c8f6f44318614e7901d881f93e585
William J. Bowman <wjb@williamjbowman.com>
2021-06-27 15:32:22 -0700

    Fix get-recent when timestamp are ill-formed

--- get-recent.py
+++ get-recent.py
@@ -4,12 +4,10 @@ from datetime import datetime, timedelta
 import tempfile
 import os
 import argparse
+import email.utils
 
 def parse_email_timestamp(raw_timestamp):
-  # Cleanup the timestamp, which might begin with a Day and a comma, and might end with a timezone declaration.
-  # First split the day off, but it might not exist, so grab the -1th element of the split (either the 1st or the 0th if there was no day).
-  # Then split off the timezone, which begins with either a + or -. Those characters ought not appear anywhere else?
-  return datetime.strptime(raw_timestamp.split(",")[-1].split("+")[0].split("-")[0].strip(), "%d %b %Y %H:%M:%S")
+  return datetime.fromtimestamp(email.utils.mktime_tz(email.utils.parsedate_tz(raw_timestamp)))
 
 parser = argparse.ArgumentParser(description='Get only the most recent files from an mbox, printing a new mbox file')
 parser.add_argument('mboxs', metavar="f", type=str, nargs='+', help="an mbox file to search")
@@ -18,6 +16,17 @@ ns = parser.parse_args()
 for mbox in ns.mboxs:
   messages = mailbox.mbox(mbox)
   for msg in messages:
-    # If the email is "recent"; use 36 hours to account for timezone issues.
-    if parse_email_timestamp(msg['Date']) > datetime.now()-timedelta(hours=36):
-      print(msg)
+    # If there is a date header, use it, otherwise, user the first line, which
+    # always contains a timestamp....
+    raw_timestamp = msg['Date']
+    if not raw_timestamp:
+      raw_timestamp = re.sub("^From \w+? ", "", str(msg).partition("\n")[0])
+    try:
+      if parse_email_timestamp(raw_timestamp) > datetime.now()-timedelta(hours=36):
+        print(msg)
+    # if parsing a timestamp fails, log it, but just ignore that message.
+    # ignoring a few messages is fine, since we're just using this for training
+    # the spam filter.
+    except Exception:
+      sys.stderr.write("Exception while parsing Timestamp: \n  " + raw_timestamp)
+      pass
