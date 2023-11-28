850a225d50ccb27ab459c1ac5827c398942b6367
William J. Bowman <wjb@williamjbowman.com>
2023-11-27 16:33:00 -0800

    Fix call to sa-compile

--- daily-sa-learn+update.sh
+++ daily-sa-learn+update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sh ~wilbowma/procmail/daily-sa-learn.sh
-umask 022 && ~/perl5/bin/sa-update && PERL_MM_OPT='' ~/perl5/bin/sa-compile
-#&& find perl5/ -mtime 1 -type d -not -path "sa-update-keys" -exec chmod g+rx {} + && find perl5/ -type f -not -path "sa-update-keys" -exec chmod g+r {} +
+umask 022 && ~/perl5/bin/sa-update && env PERL_MM_OPT=' sa-compile' ~/perl5/bin/sa-compile
+# && find perl5/ -mtime 1 -type d -not -path "sa-update-keys" -exec chmod g+rx {} + && find perl5/ -type f -not -path "sa-update-keys" -exec chmod g+r {} +
